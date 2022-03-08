62dcd39c6efdf822af59eb681e479817a6b0dcdb
Prins <88283873+prins2003@users.noreply.github.com>
2022-03-08 21:50:41 +0530

    printLL java Program

---
+++ LinkedList/printLL.java
@@ -0,0 +1,8 @@
+public static void printLL2(Node<Integer> head){
+        Node<Integer> temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
