06ca6c61d4818518a3e8eca74f9cac2a2df072e8
Prins <88283873+prins2003@users.noreply.github.com>
2022-03-11 21:51:17 +0530

    Create printNodeAtDepthK.java

---
+++ Tree/printNodeAtDepthK.java
@@ -0,0 +1,11 @@
+public static void printAtDepthK(BinaryTreeNode<Integer> root, int k){
+        if(root==null){
+            return;
+        }
+        if(k==0){
+            System.out.println(root.data);
+            return;
+        }
+        printAtDepthK(root.left,k-1);
+        printAtDepthK(root.right,k-1);
+    }
