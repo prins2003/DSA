f7110af485f5a093dff85f8e51868dfe06368873
Prins <88283873+prins2003@users.noreply.github.com>
2022-03-11 18:58:38 +0530

    Create sumOfNode.java

---
+++ Tree/sumOfNode.java
@@ -0,0 +1,9 @@
+static int sumBT(Node head){
+        //Code
+        if(head==null){
+            return 0;
+        }
+        int leftSum = sumBT(head.left);
+        int rightSum = sumBT(head.right);
+        return leftSum + rightSum + head.data;
+    }
