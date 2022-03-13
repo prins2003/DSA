fd23a9951874493a47dc5256e3be97ee84082427
Prins <88283873+prins2003@users.noreply.github.com>
2022-03-13 14:57:30 +0530

    Update diameterOfTree.java

--- Tree/diameterOfTree.java
+++ Tree/diameterOfTree.java
@@ -17,3 +17,39 @@ public static int diameter(BinaryTreeNode<Integer> root){
         int option3 = diameter(root.right);
         return Math.max(option1,Math.max(option2,option3));
 }
+
+
+// effiecint :
+class Pair {
+    int height;
+    int diameter;
+    Pair(int height,int diameter){
+        this.height=height;
+        this.diameter=diameter;
+    }
+}
+
+private static Pair daimeterHelper(BinaryTreeNode<Integer> root){
+        if(root==null){
+                Pair pair = new Pair(0,0);
+                return pair;
+        }
+        
+        Pair leftPair = daimeterHelper(root.left);
+        Pair rightPair = daimeterHelper(root.right);
+        
+        int leftDiameter = leftPair.diameter;
+        int rightDiameter = rightPair.diameter;
+        
+        int dis = leftPair.height + rightPair.height + 1;
+        
+        int diameter = Math.max(leftDiameter,Math.max(rightDiameter,dist));
+        int height = Math.max(leftPair.height,rightPair.height)+1;
+        
+        return new Pair(diameter,height);
+}
+
+public static int diameterOfBinaryTree(BinaryTreeNode<Integer> root){
+        Pair pair = diameterHelper(root);
+        return pair.diameter;
+}
