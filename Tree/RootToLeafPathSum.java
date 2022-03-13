a71f3ab80b4cffff2f91705d6c461c81498c9605
Prins <88283873+prins2003@users.noreply.github.com>
2022-03-13 14:05:19 +0530

    Update RootToLeafPathSum.java

--- Tree/RootToLeafPathSum.java
+++ Tree/RootToLeafPathSum.java
@@ -16,3 +16,27 @@ public static boolean helper(Node root,int S,int sum){
         // Your code here
         return helper(root,S,0);
     }
+
+
+// Print Path
+
+private static void rootToLeafPathSumHelper(BinaryTreeNode<Integer> root,int k,String path,int currSum){
+        if(root=null){
+                return;
+        }
+        if(root.left==null && root.right==null){
+                currSum += root.data;
+                if(currSum==k){
+                        System.out.println((path+root.data+" "));
+                }
+                return;
+        }
+        rootToLeafPathSumHelper(root.left,k,(path+root.data+" "),currSum+root.data);
+        rootToLeafPathSumHelper(root.right,k,(path+root.data+" "),currSum+root.data);
+}
+
+public static void rootToLeafPathSum(BinaryTreeNode<Integer> root,int k){
+        rootToLeafPathSumHelper(root,k,"",0);
+}
+
+}
