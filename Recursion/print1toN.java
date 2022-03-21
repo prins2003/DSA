cc529dee31d43aa1303dcb2d55369af4a6792ec5
Prins <88283873+prins2003@users.noreply.github.com>
2022-03-21 22:27:08 +0530

    Create print1toN.java

---
+++ Recursion/print1toN.java
@@ -0,0 +1,8 @@
+public static void print(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        print(n-1);
+        System.out.print(n+" ");
+    }
